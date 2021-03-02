466f06980fcc71b848ce51745a6c0a25f2d101a9
zhanghuizong <zhanghzong@163.com>
2021-03-02 20:28:39 +0800

    监控所有 Java 接口

--- service/javaApi/curl.go
+++ service/javaApi/curl.go
@@ -2,6 +2,7 @@ package javaApi
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/wenzhenxi/gorsa"
 	"github.com/zhanghuizong/bitgame/service/config"
@@ -43,11 +44,13 @@ func post(api string, data map[string]interface{}) (string, error) {
 
 	bodyJson, mErr := json.Marshal(reqData)
 	if mErr != nil {
+		go SendMessageFormat(url, dataStr, mErr)
 		return "", mErr
 	}
 
 	req, rErr := http.NewRequest("POST", url, strings.NewReader(string(bodyJson)))
 	if rErr != nil {
+		go SendMessageFormat(url, dataStr, rErr)
 		return "", rErr
 	}
 
@@ -57,6 +60,7 @@ func post(api string, data map[string]interface{}) (string, error) {
 	resp, clientErr := client.Do(req)
 	if clientErr != nil {
 		logrus.Warnln("POST 请求接口异常", clientErr, url)
+		go SendMessageFormat(url, dataStr, clientErr)
 		return "", clientErr
 	}
 
@@ -65,6 +69,7 @@ func post(api string, data map[string]interface{}) (string, error) {
 	body, readErr := ioutil.ReadAll(resp.Body)
 	if readErr != nil {
 		logrus.Warnln("POST 读取响应数据异常", readErr, url)
+		go SendMessageFormat(url, dataStr, readErr)
 		return "", readErr
 	}
 
@@ -72,4 +77,28 @@ func post(api string, data map[string]interface{}) (string, error) {
 	logrus.Infof("请求响应:%s, url:%s", bodyStr, url)
 
 	return bodyStr, nil
+}
+
+// 异常报警
+func SendMessageFormat(url string, dataStr string, err error) {
+	errStr := ""
+	if err != nil {
+		errStr = err.Error()
+	}
+
+	txt := "游戏编号：%s\n" +
+		"日期：%s\n" +
+		"接口地址：%s\n" +
+		"请求参数：%s\n" +
+		"异常：接口请求异常"
+
+	// 拼接错误数据
+	if errStr != "" {
+		txt = txt + ". err:" + errStr
+	}
+
+	gameNo := config.GetJavaGameId()
+	now := time.Now().Format("2006-01-02 15:04:05")
+	txt = fmt.Sprintf(txt, gameNo, now, url, dataStr)
+	_, _ = SendMessage(txt)
 }
